f0bdcce665de9c8b1a281165ba4575aeedd0bf55
matteobuscicchio <matteoezio@me.com>
2021-01-10 18:55:24 +0100

    eliminato il console log dal file main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,15 +10,14 @@ let app = new Vue({
             for (var index = 0; index < number; index++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then(function(index){
-                    app.eMailArray.push(index.data.response);
-                    console.log(index.data.response);
+                    app.eMailArray.push(index.data.response)
                 })
             }
       
         },
 
     },
-    
+
     mounted(){
         const request = 10;
         this.eMailArrayGeneartor(request);
